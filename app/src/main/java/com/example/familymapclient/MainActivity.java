b8d54d1e58081119ac8907a4c37d6388f63650e4
Hayden Parkinson <haydendparkinson@gmail.com>
2019-03-19 16:12:00 -0600

    building login fragment view

--- app/src/main/java/com/example/familymapclient/MainActivity.java
+++ app/src/main/java/com/example/familymapclient/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.familymapclient;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        //  Add LoginFragment if user is not logged in
+        createLoginFragment();
+        //  Add MapFragment if user is logged in
+    }
+
+    private void createLoginFragment() {
+        FragmentManager fm = this.getSupportFragmentManager();
+        LoginFragment loginFragment = (LoginFragment) fm.findFragmentById(R.id.mainFrameLayout);
+        if ( loginFragment == null ){
+            loginFragment = LoginFragment.newInstance();
+            fm.beginTransaction().add(R.id.mainFrameLayout, loginFragment).commit();
+        }
     }
 }
